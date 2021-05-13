c0ca1098b29b3bd97e980c2fe96a6de6c6c13b08
Maxel Schultz <mschultz@punchthrough.com>
2021-05-13 17:03:15 -0500

    Committing script I use to install software on a new workstation

---
+++ install_chocolatey_packages.ps1
@@ -0,0 +1,7 @@
+#Script running was disabled on my system. Had to set execution policy to allsigned
+#Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install .\max_choco.config -y
+
+refreshenv
